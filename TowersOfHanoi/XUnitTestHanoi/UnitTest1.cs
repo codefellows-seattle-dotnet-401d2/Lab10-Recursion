7c0bb2b638e7f3e93e7bf19d8a107e873953541d
Ariel R. Pedraza <pedrazaa2@gmail.com>
2018-01-08 11:11:54 -0800

    Created for loop version for iterative solution. Created README.

--- TowersOfHanoi/XUnitTestHanoi/UnitTest1.cs
+++ TowersOfHanoi/XUnitTestHanoi/UnitTest1.cs
@@ -12,9 +12,20 @@ namespace XUnitTestHanoi
         [InlineData (5, 31)]
         [InlineData (6, 63)]
         [InlineData (7, 127)]
-        public void Test1(int n, int answer)
+        public void TestRecursion(int n, int answer)
         {
             Assert.Equal(answer, Program.Hanoi(n));
+        }
+
+        [Theory]
+        [InlineData(3, 7)]
+        [InlineData(4, 15)]
+        [InlineData(5, 31)]
+        [InlineData(6, 63)]
+        [InlineData(7, 127)]
+        public void TestIterative(int n, int answer)
+        {
+            Assert.Equal(answer, Program.Hanoi2(n));
         }
     }
 }
