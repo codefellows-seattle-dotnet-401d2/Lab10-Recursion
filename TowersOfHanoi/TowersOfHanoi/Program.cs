eb75f8079a69ec4f4a065398e9844be38df11f7c
QueensKid <andres.ibarra0716@gmail.com>
2018-01-08 22:21:07 -0800

    Had to commit twice to have all changes reflect on github.

---
+++ TowersOfHanoi/TowersOfHanoi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TowersOfHanoi
+{
+    class Gameplay
+    {
+        public static int Main()
+        {
+            TowerOfHanoi Tower = new TowerOfHanoi();
+            string userNumDiscs;
+
+            Console.Write("Enter the number of discs you wish to play with: ");
+            userNumDiscs = Console.ReadLine();
+            Tower.Numdiscs = Convert.ToInt32(userNumDiscs);
+
+            Tower.MoveTower(Tower.Numdiscs, 1, 3, 2);
+            Console.ReadLine();
+
+            return 0;
+        }
+    }
+}
